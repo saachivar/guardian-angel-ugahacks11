d3d20002d3b386d1323b3212ada80a858916ff08
Saachi Varshney <140358937+saachivar@users.noreply.github.com>
2026-02-06 22:31:32 -0500

    created file that connects to database

---
+++ Backend/database.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://db_user:db_pass@guardian-angel.k4xhwty.mongodb.net/")
+
+# Access a database (doesn't exist yet on server)
+db = client["my_database"]
+
+# Access a collection (like a table)
+collection = db["users"]
+
+# Insert a document (this triggers database creation)
+collection.insert_one({"name": "Aasdfadsfe", "age": 25})
+
+print("Inserted document! Database should now exist in MongoDB Atlas.")
--- Backend/main.py
+++ Backend/main.py
@@ -3,6 +3,7 @@
     from firebase_functions import https
     from firebase_admin import initialize_app
 
+
     initialize_app()
     app = Flask(__name__)
 
@@ -14,9 +15,3 @@
     def get_data():
         return jsonify({"message": "This is your API data!"})
 
-    # This is the entry point for your Cloud Function
-    @https.on_request()
-    def my_flask_app(req: https.Request) -> https.Response:
-        with app.request_context(req.environ):
-            return app.full_dispatch_request()
-    ```
